5670d94d1635cba7b3b04cb3953124af5f1a6687
NuggetReckt <84108267+NuggetReckt@users.noreply.github.com>
2023-12-23 16:51:40 +0100

    Improved: Loading embeds on persistent messages

--- src/main/java/fr/nuggetreckt/nswbot/tasks/MessagesSender.java
+++ src/main/java/fr/nuggetreckt/nswbot/tasks/MessagesSender.java
@@ -17,7 +17,15 @@ public class MessagesSender {
     final MessageChannel supportChannel = NSWBot.getConfig().getSupportChannel();
     final MessageChannel ticketChannel = NSWBot.getConfig().getTicketPanel();
 
+    private EmbedBuilder rules;
+    private EmbedBuilder support;
+    private EmbedBuilder panel;
+
     public MessagesSender() {
+        this.setRuleEmbed();
+        this.setSupportEmbed();
+        this.setPanelEmbed();
+
         this.sendRulesMessage();
         this.sendSupportMessage();
         this.sendTicketPanelMessage();
@@ -30,25 +38,6 @@ public class MessagesSender {
         if (messages.size() == 1) return;
 
         if (messages.isEmpty()) {
-            EmbedBuilder rules = new EmbedBuilder();
-            rules.setTitle("✅ ・ Règles", "https://play.noskillworld.fr/cgu-cgv")
-                    .setDescription("En lisant ces règles ci-dessous, vous vous engagez à les respecter et à vous exposer à des **sanctions** dans le cas contraire.")
-                    .addField("<:info_nsw:864197429729034250> __Sur le Discord__", """
-                            🔹Respectez-vous les uns des autres
-                            🔹Pas d'insultes
-                            🔹Pas de pub
-                            🔹Pas de spam
-                            🔹Pas de contenu nsfw/raciste/homophobe/...
-                            """, true)
-                    .addField("<:info_nsw:864197429729034250> __En jeu__", """
-                            Retrouvez les règles en jeu complètes ici : https://play.noskillworld.fr/règles
-                                                        
-                            🔹Vous êtes témoin d'un tp kill, cheat, insultes, ou grief ? Créez un ticket.
-                            """ + NSWBot.getConfig().getTicketPanel().getAsMention(), true)
-                    .setColor(new Color(61, 189, 201, 1))
-                    .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
-                    .setTimestamp(new Date().toInstant());
-
             rulesChannel.sendMessageEmbeds(rules.build()).queue();
         }
     }
@@ -60,33 +49,6 @@ public class MessagesSender {
         if (messages.size() == 1) return;
 
         if (messages.isEmpty()) {
-            EmbedBuilder support = new EmbedBuilder();
-            support.setTitle("🔗 ・ Nous Soutenir", "https://play.noskillworld.fr")
-                    .addField("<:info_nsw:864197429729034250> __Introduction__", """
-                            Vous aimez le serveur et vous souhaitez nous aider ?
-                            Si c'est le cas vous êtes au bon endroit !
-                            Deux moyens s'offrent à vous pour nous soutenir :
-                            \u00A0\u00A0\u00A0 - A l'aide des sites de référencement pour nous permettre de gagner en visibilité
-                            \u00A0\u00A0\u00A0 - En achetant un grade ou en faisant un don
-                            Merci à ceux qui le feront, ça aide grandement le serveur et permet de le garder ouvert ! ❤️
-                            """, false)
-                    .addField("<:info_nsw:864197429729034250> __Sites de référencement__", """
-                            🔹**Serveurs-minecraft.org :** https://www.serveurs-minecraft.org/vote.php?id=60934
-                            🔹**Serveur-minecraft.com :** https://serveur-minecraft.com/2598
-                            🔹**Serveurs-mc.net :** https://serveurs-mc.net/index.php/serveur/251
-                            🔹**Top-serveurs.net :** https://top-serveurs.net/minecraft/vote/noskillworld
-                            🔹**Serveur-privé.net :** https://serveur-prive.net/minecraft/noskillworld-9550/vote
-                            🔹**Serveursminecraft.org :** https://www.serveursminecraft.org/serveur/5870/
-                            🔹**Liste-serveurs-minecraft.org :** https://www.liste-serveurs-minecraft.org/serveur-minecraft/noskillworld/
-                            """, false)
-                    .addField("<:info_nsw:864197429729034250> __Grades/Dons__", """
-                            🛒 ・ **Shop :** https://shop.noskillworld.fr
-                            💎 ・ **PayPal :** https://paypal.me/noskillworld
-                            """, false)
-                    .setColor(new Color(61, 189, 201, 1))
-                    .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
-                    .setTimestamp(new Date().toInstant());
-
             supportChannel.sendMessageEmbeds(support.build()).queue();
         }
     }
@@ -95,21 +57,14 @@ public class MessagesSender {
         MessageHistory history = MessageHistory.getHistoryFromBeginning(ticketChannel).complete();
         List<Message> messages = history.getRetrievedHistory();
 
-        EmbedBuilder panel = new EmbedBuilder();
-
-        panel.setTitle("📮 ・ Ouvrir un ticket", "https://play.noskillworld.fr")
-                .setDescription("Pour créer un ticket, cliquez sur le bouton ci-dessous.\nTout abus/troll sera sanctionné.")
-                .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
-                .setColor(new Color(61, 189, 201, 1))
-                .setTimestamp(new Date().toInstant());
-
         if (messages.size() == 1) return;
 
         if (messages.isEmpty()) {
             ticketChannel.sendMessageEmbeds(panel.build())
                     .setActionRow(
                             Button.primary("create", "Créer un ticket"),
-                            Button.link("https://play.noskillworld.fr/cgu-cgv", "Informations")
+                            Button.link("https://play.noskillworld.fr/cgu-cgv", "CGU-CGV"),
+                            Button.link("https://play.noskillworld.fr/wiki/questions-reponses", "Questions/Réponses")
                     )
                     .queue();
         }
@@ -118,38 +73,100 @@ public class MessagesSender {
     public void disableTicketCreation() {
         MessageHistory history = MessageHistory.getHistoryFromBeginning(ticketChannel).complete();
         List<Message> messages = history.getRetrievedHistory();
-
         String embed = messages.get(0).getId();
-        EmbedBuilder panel = new EmbedBuilder();
 
-        panel.setTitle("📮 ・ Ouvrir un ticket", "https://play.noskillworld.fr")
-                .setDescription("La création de ticket est temporairement désactivée.\nNous revenons bientôt.")
-                .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
-                .setColor(new Color(61, 189, 201, 1))
-                .setTimestamp(new Date().toInstant());
+        panel.setDescription("""
+                La création de ticket est temporairement désactivée.
+                Nous revenons bientôt.""");
 
         ticketChannel.editMessageEmbedsById(embed, panel.build()).setActionRow(
                 Button.primary("create", "Créer un ticket").asDisabled(),
-                Button.link("https://play.noskillworld.fr/cgu-cgv", "Informations")
+                Button.link("https://play.noskillworld.fr/cgu-cgv", "CGU-CGV"),
+                Button.link("https://play.noskillworld.fr/wiki/questions-reponses", "Questions/Réponses")
         ).queue();
     }
 
     public void enableTicketCreation() {
         MessageHistory history = MessageHistory.getHistoryFromBeginning(ticketChannel).complete();
         List<Message> messages = history.getRetrievedHistory();
-
         String embed = messages.get(0).getId();
-        EmbedBuilder panel = new EmbedBuilder();
 
-        panel.setTitle("📮 ・ Ouvrir un ticket", "https://play.noskillworld.fr")
-                .setDescription("Pour créer un ticket, cliquez sur le bouton ci-dessous.\nTout abus/troll sera sanctionné.")
-                .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
-                .setColor(new Color(61, 189, 201, 1))
-                .setTimestamp(new Date().toInstant());
+        panel.setDescription("""
+                Pour créer un ticket, cliquez sur le bouton ci-dessous.
+                Votre question se trouve peut-être sur la page des Questions/Réponses. Jetez-y un oeil avant de créer votre ticket.
+                                        
+                Tout abus/troll sera sanctionné.""");
 
         ticketChannel.editMessageEmbedsById(embed, panel.build()).setActionRow(
                 Button.primary("create", "Créer un ticket").asEnabled(),
-                Button.link("https://play.noskillworld.fr/cgu-cgv", "Informations")
+                Button.link("https://play.noskillworld.fr/cgu-cgv", "CGU-CGV"),
+                Button.link("https://play.noskillworld.fr/wiki/questions-reponses", "Questions/Réponses")
         ).queue();
+    }
+
+    public void setRuleEmbed() {
+        rules = new EmbedBuilder();
+
+        rules.setTitle("✅ ・ Règles", "https://play.noskillworld.fr/cgu-cgv")
+                .setDescription("En lisant ces règles ci-dessous, vous vous engagez à les respecter et à vous exposer à des **sanctions** dans le cas contraire.")
+                .addField("<:info_nsw:864197429729034250> __Sur le Discord__", """
+                        🔹Respectez-vous les uns des autres
+                        🔹Pas d'insultes
+                        🔹Pas de pub
+                        🔹Pas de spam
+                        🔹Pas de contenu nsfw/raciste/homophobe/...
+                        """, true)
+                .addField("<:info_nsw:864197429729034250> __En jeu__", """
+                        Retrouvez les règles en jeu complètes ici : https://play.noskillworld.fr/règles
+                                                    
+                        🔹Vous êtes témoin d'un tp kill, cheat, insultes, ou grief ? Créez un ticket.
+                        """ + NSWBot.getConfig().getTicketPanel().getAsMention(), true)
+                .setColor(new Color(61, 189, 201, 1))
+                .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
+                .setTimestamp(new Date().toInstant());
+    }
+
+    public void setSupportEmbed() {
+        support = new EmbedBuilder();
+
+        support.setTitle("🔗 ・ Nous Soutenir", "https://play.noskillworld.fr")
+                .addField("<:info_nsw:864197429729034250> __Introduction__", """
+                        Vous aimez le serveur et vous souhaitez nous aider ?
+                        Si c'est le cas vous êtes au bon endroit !
+                        Deux moyens s'offrent à vous pour nous soutenir :
+                        \u00A0\u00A0\u00A0 - A l'aide des sites de référencement pour nous permettre de gagner en visibilité
+                        \u00A0\u00A0\u00A0 - En achetant un grade ou en faisant un don
+                        Merci à ceux qui le feront, ça aide grandement le serveur et permet de le garder ouvert ! ❤️
+                        """, false)
+                .addField("<:info_nsw:864197429729034250> __Sites de référencement__", """
+                        🔹**Serveurs-minecraft.org :** https://www.serveurs-minecraft.org/vote.php?id=60934
+                        🔹**Serveur-minecraft.com :** https://serveur-minecraft.com/2598
+                        🔹**Serveurs-mc.net :** https://serveurs-mc.net/index.php/serveur/251
+                        🔹**Top-serveurs.net :** https://top-serveurs.net/minecraft/vote/noskillworld
+                        🔹**Serveur-privé.net :** https://serveur-prive.net/minecraft/noskillworld-9550/vote
+                        🔹**Serveursminecraft.org :** https://www.serveursminecraft.org/serveur/5870/
+                        🔹**Liste-serveurs-minecraft.org :** https://www.liste-serveurs-minecraft.org/serveur-minecraft/noskillworld/
+                        """, false)
+                .addField("<:info_nsw:864197429729034250> __Grades/Dons__", """
+                        🛒 ・ **Shop :** https://shop.noskillworld.fr
+                        💎 ・ **PayPal :** https://paypal.me/noskillworld
+                        """, false)
+                .setColor(new Color(61, 189, 201, 1))
+                .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
+                .setTimestamp(new Date().toInstant());
+    }
+
+    public void setPanelEmbed() {
+        panel = new EmbedBuilder();
+
+        panel.setTitle("📮 ・ Ouvrir un ticket", "https://play.noskillworld.fr")
+                .setDescription("""
+                        Pour créer un ticket, cliquez sur le bouton ci-dessous.
+                        Votre question se trouve peut-être sur la page des Questions/Réponses. Jetez-y un oeil avant de créer votre ticket.
+                                                
+                        Tout abus/troll sera sanctionné.""")
+                .setFooter("NSW - Semi-RP", "https://play.noskillworld.fr/assets/images/embed-icon.png")
+                .setColor(new Color(61, 189, 201, 1))
+                .setTimestamp(new Date().toInstant());
     }
 }
